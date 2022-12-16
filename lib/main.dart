f6e0b9a513158ec41b1ac9451756a12de6eb7410
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2022-12-16 14:07:30 +0900

    Segup code for the tutorial "Adding interactivity to your Flutter app"

--- lib/main.dart
+++ lib/main.dart
@@ -40,7 +40,7 @@ class MyApp extends StatelessWidget {
 
   Widget _createTitleSection() {
     return Container(
-      decoration: const BoxDecoration(color: Color.fromARGB(50, 255, 0, 0)),
+      // decoration: const BoxDecoration(color: Color.fromARGB(50, 255, 0, 0)),
       padding: const EdgeInsets.all(32),
       child: Row(
         children: [
@@ -82,16 +82,13 @@ class MyApp extends StatelessWidget {
   }
 
   Widget _createButtonSection([Color color = Colors.orange]) {
-    return Container(
-      decoration: const BoxDecoration(color: Colors.pink),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          _buildButtonColumn(color, Icons.call, 'CALL'),
-          _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
-          _buildButtonColumn(color, Icons.share, 'SHARE'),
-        ],
-      ),
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'CALL'),
+        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(color, Icons.share, 'SHARE'),
+      ],
     );
   }
 
@@ -117,19 +114,16 @@ class MyApp extends StatelessWidget {
   }
 
   Widget _createTextSection() {
-    return Container(
-      decoration: const BoxDecoration(color: Colors.yellow),
-      child: const Padding(
-        padding: EdgeInsets.all(32),
-        child: Text(
-          'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
-          'Alps. Situated 1,578 meters above sea level, it is one of the '
-          'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
-          'half-hour walk through pastures and pine forest, leads you to the '
-          'lake, which warms to 20 degrees Celsius in the summer. Activities '
-          'enjoyed here include rowing, and riding the summer toboggan run.',
-          softWrap: true,
-        ),
+    return const Padding(
+      padding: EdgeInsets.all(32),
+      child: Text(
+        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+        'Alps. Situated 1,578 meters above sea level, it is one of the '
+        'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+        'half-hour walk through pastures and pine forest, leads you to the '
+        'lake, which warms to 20 degrees Celsius in the summer. Activities '
+        'enjoyed here include rowing, and riding the summer toboggan run.',
+        softWrap: true,
       ),
     );
   }
