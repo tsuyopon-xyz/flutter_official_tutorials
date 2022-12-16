873a94539cb470b01475af775d1c852957848958
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2022-12-16 17:36:28 +0900

    Done example4

--- lib/main.dart
+++ lib/main.dart
@@ -2,24 +2,6 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(const LogoApp());
 
-class AnimatedLogo extends AnimatedWidget {
-  const AnimatedLogo({super.key, required Animation<double> animation})
-      : super(listenable: animation);
-
-  @override
-  Widget build(BuildContext context) {
-    final animation = listenable as Animation<double>;
-    return Center(
-      child: Container(
-        margin: const EdgeInsets.symmetric(vertical: 10),
-        height: animation.value,
-        width: animation.value,
-        child: const FlutterLogo(),
-      ),
-    );
-  }
-}
-
 class LogoApp extends StatefulWidget {
   const LogoApp({super.key});
 
@@ -36,42 +18,59 @@ class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
     super.initState();
     controller =
         AnimationController(duration: const Duration(seconds: 2), vsync: this);
-    // animation = Tween<double>(begin: 0, end: 300).animate(controller)
-    //   ..addListener(() {
-    //     setState(() {
-    //       // The state that has changed here is the animation object’s value.
-    //     });
-    //   });
-    // animation = Tween<double>(begin: 0, end: 300).animate(controller);
     animation = Tween<double>(begin: 0, end: 300).animate(controller);
-    animation.addStatusListener((status) {
-      print('Called : $status');
-      if (status == AnimationStatus.completed) {
-        controller.reverse();
-      } else if (status == AnimationStatus.dismissed) {
-        controller.forward();
-      }
-    });
     controller.forward();
   }
 
   @override
-  // Widget build(BuildContext context) {
-  //   return Center(
-  //     child: Container(
-  //       margin: const EdgeInsets.symmetric(vertical: 10),
-  //       height: animation.value,
-  //       width: animation.value,
-  //       decoration: const BoxDecoration(color: Colors.teal),
-  //       child: const FlutterLogo(),
-  //     ),
-  //   );
-  // }
-  Widget build(BuildContext context) => AnimatedLogo(animation: animation);
+  Widget build(BuildContext context) {
+    return GrowTransition(
+      animation: animation,
+      child: const LogoWidget(),
+    );
+  }
 
   @override
   void dispose() {
     controller.dispose();
     super.dispose();
+  }
+}
+
+class LogoWidget extends StatelessWidget {
+  const LogoWidget({super.key});
+
+  // Leave out the height and width so it fills the animating parent
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      child: const FlutterLogo(),
+    );
+  }
+}
+
+class GrowTransition extends StatelessWidget {
+  const GrowTransition(
+      {required this.child, required this.animation, super.key});
+
+  final Widget child;
+  final Animation<double> animation;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: AnimatedBuilder(
+        animation: animation,
+        builder: (context, child) {
+          return SizedBox(
+            height: animation.value,
+            width: animation.value,
+            child: child,
+          );
+        },
+        child: child,
+      ),
+    );
   }
 }
